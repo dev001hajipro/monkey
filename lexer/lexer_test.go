082f0ee2a20cc94d405c55bd150a68c6e9f2ed6b
dev001hajipro <dev001hajipro@gmail.com>
2022-05-22 07:14:25 +0000

    add test for GreatThen or LettleThan

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -102,6 +102,20 @@ let result = add(five, ten);
 !-/*5;
 5 < 10 > 5;`
 */
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		
 
 		{token.EOF, ""},
 	}
