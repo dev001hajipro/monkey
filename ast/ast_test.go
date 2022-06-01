25f5441187de1b3e4e26b6a76f9261735261a32b
dev001hajipro <dev001hajipro@gmail.com>
2022-06-01 08:21:04 +0000

    add ast demonstration by hand

---
+++ ast/ast_test.go
@@ -0,0 +1,32 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/dev001hajipro/monkey/token"
+)
+
+// demo
+// if code is 'let myVar = anotherVar;'
+// AST become follow code.
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
