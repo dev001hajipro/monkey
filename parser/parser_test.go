fc9631fea77e9ded80fd7933fe7dcd2fe9ee3b06
dev001hajipro <dev001hajipro@gmail.com>
2022-06-01 09:46:58 +0000

    add identifierExpression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/dev001hajipro/monkey/lexer"
 )
 
-
 func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
 	if s.TokenLiteral() != "let" {
 		t.Errorf("s.TokenLiteral not 'let'. got=%q", s.TokenLiteral())
@@ -103,6 +102,35 @@ let 838383;
 	for _, msg := range p.errors {
 		t.Logf("parser error: %q", msg)
 	}
-	
+
 }
 
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. got=%d",
+			len(program.Statements))
+	}
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T",
+			program.Statements[0])
+	}
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("exp not *ast.Identifier. got=%T", stmt.Expression)
+	}
+	if ident.Value != "foobar" {
+		t.Fatalf("ident.Value not %s. got=%s", "foobar", ident.Value)
+	}
+	if ident.TokenLiteral() != "foobar" {
+		t.Fatalf("ident.TokenLiteral() not %s. got=%s",
+			"foobar", ident.TokenLiteral())
+	}
+}
