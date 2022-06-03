533f58173d27d76b5329ec730d8a61e55da7750d
dev001hajipro <dev001hajipro@gmail.com>
2022-06-04 06:00:09 +0900

    add parserIntegerLiteral

--- ast/ast.go
+++ ast/ast.go
@@ -69,6 +69,15 @@ func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 func (i *Identifier) String() string       { return i.Value }
 
+type IntegerLiteral struct {
+	Token token.Token // token.INT
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
+
 type ReturnStatement struct {
 	Token       token.Token // token.RETURN
 	ReturnValue Expression
