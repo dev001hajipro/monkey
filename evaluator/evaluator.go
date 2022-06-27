d854959d633d543db65cdf6946ceba3b9805c255
dev001hajipro <dev001hajipro@gmail.com>
2022-06-27 13:26:45 +0900

    add to eval infix expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -14,6 +14,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
@@ -42,6 +46,37 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 		return NULL
 	}
 }
+func evalInfixExpression(
+	operator string,
+	left, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(
+	operator string,
+	left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+	
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	default:
+		return NULL
+	}
+}
 
 func evalBangOperatorExpression(right object.Object) object.Object {
 	switch right {
@@ -61,7 +96,7 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 		return NULL
 	}
 	value := right.(*object.Integer).Value
-	return &object.Integer{Value:-value} // value to negative
+	return &object.Integer{Value: -value} // value to negative
 }
 
 func nativeBooleanBoject(input bool) *object.Boolean {
