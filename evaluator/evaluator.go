639ebf9caaac28d7fa3d99adac49bb193b6d9f01
dev001hajipro <dev001hajipro@gmail.com>
2022-06-27 13:47:41 +0900

    add to eval == and !=

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -46,13 +46,17 @@ func evalPrefixExpression(operator string, right object.Object) object.Object {
 		return NULL
 	}
 }
+
 func evalInfixExpression(
 	operator string,
 	left, right object.Object) object.Object {
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
-
+	case operator == "==":
+		return nativeBooleanBoject(left == right)
+	case operator == "!=":
+		return nativeBooleanBoject(left != right)
 	default:
 		return NULL
 	}
@@ -63,7 +67,7 @@ func evalIntegerInfixExpression(
 	left, right object.Object) object.Object {
 	leftVal := left.(*object.Integer).Value
 	rightVal := right.(*object.Integer).Value
-	
+
 	switch operator {
 	case "+":
 		return &object.Integer{Value: leftVal + rightVal}
@@ -73,6 +77,14 @@ func evalIntegerInfixExpression(
 		return &object.Integer{Value: leftVal * rightVal}
 	case "/":
 		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBooleanBoject(leftVal < rightVal)
+	case ">":
+		return nativeBooleanBoject(leftVal > rightVal)
+	case "==":
+		return nativeBooleanBoject(leftVal == rightVal)
+	case "!=":
+		return nativeBooleanBoject(leftVal != rightVal)
 	default:
 		return NULL
 	}
