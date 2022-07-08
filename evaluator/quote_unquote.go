33ad202e9ce61407674c48a3c6f4af1b13f81fa7
dev001hajipro <dev001hajipro@gmail.com>
2022-07-08 18:59:00 +0900

    add the modify function

--- evaluator/quote_unquote.go
+++ evaluator/quote_unquote.go
@@ -5,6 +5,7 @@ import (
 	"github.com/dev001hajipro/monkey/object"
 )
 
+// Node object of the argument is wrapped in a Quote object
 func quote(node ast.Node) object.Object {
 	return &object.Quote{Node: node}
 }
